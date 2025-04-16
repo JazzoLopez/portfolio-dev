412c2f426735100d19aec8d9e1619829ebf75534
JazzoLopez <jazzielrodriguezlopez@gmail.com>
2025-04-16 13:04:39 -0600

    fix: Update work experiences in the experience component feat: Fix border styling in the Experience component

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -2,7 +2,7 @@ import { EXPERIENCES } from "../constants";
 
 export default function Experience() {
     return (
-        <div className="border-2 border-neutral-900 pb-4">
+        <div className="border-b border-neutral-900 pb-4">
             <h2 className="my-20 text-center text-4xl">Experiencia</h2>
             <div className="">
                 {EXPERIENCES.map((experience, i) => (
