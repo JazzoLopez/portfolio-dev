f89bb41888848fe07d08c6da2d9ae002c7701585
JazzoLopez <jazzielrodriguezlopez@gmail.com>
2025-04-16 12:49:45 -0600

    feat: Add Experience component to display user experiences

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import NavBar from './components/NavBar'
 import Hero from './components/Hero'
 import About from './components/About'
 import Technologies from './components/Technologies'
+import Experience from './components/Experience'
 
 export default function App() {
   return (
@@ -17,6 +18,7 @@ export default function App() {
         <Hero />
         <About />
         <Technologies />
+        <Experience />
       </div>
     </div>
   )
