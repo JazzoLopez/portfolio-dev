dbded8436e0a8060ba26eec60a724b57fa9930a3
JazzoLopez <jazzielrodriguezlopez@gmail.com>
2025-04-16 21:23:29 -0600

    feat: refactor NavBar and Technologies components for improved structure and animations

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,22 +1,30 @@
-import logo from '../assets/logo.jpeg'
-import { FaLinkedin } from 'react-icons/fa'
-import { FaGithub } from 'react-icons/fa'
-import { FaTwitter } from 'react-icons/fa'
-import { FaInstagram } from 'react-icons/fa'
+import { FaLinkedin, FaGithub, FaTwitter, FaInstagram } from 'react-icons/fa'
 
 export default function NavBar() {
     return (
         <div className="mb-20 flex items-center justify-between py-6">
             <div className="flex flex-shrink-0 items-center">
-                {/* <img src={logo} alt="logo" width={50} height={50} className='mx-2 w-10' /> */}
-                <h1 className='text-5xl'>J</h1><h2 className='text-4xl'>L</h2>
+                <h1 className='text-5xl text-white'>J</h1><h2 className='text-4xl text-white'>L</h2>
             </div>
-            <div className="m-8 flex items-center justify-center gap-4 text-2xl">
-                <FaLinkedin />
-                <FaGithub />
-                <FaTwitter />
-                <FaInstagram />
-
+            <div className="m-8 flex items-center justify-center gap-4 text-2xl text-gray-300">
+                <a href="https://www.linkedin.com/in/jazziel-rodriguez-lopez-9b9b362ab/" 
+                   target="_blank" 
+                   rel="noopener noreferrer" 
+                   className="hover:text-blue-500 transition-colors">
+                    <FaLinkedin />
+                </a>
+                <a href="https://github.com/JazzoLopez" 
+                   target="_blank" 
+                   rel="noopener noreferrer" 
+                   className="hover:text-white transition-colors">
+                    <FaGithub />
+                </a>
+                <a href="https://x.com/JazzieloRL" 
+                   target="_blank" 
+                   rel="noopener noreferrer" 
+                   className="hover:text-sky-400 transition-colors">
+                    <FaTwitter />
+                </a>
             </div>
         </div>
     )
--- src/components/Technologies.jsx
+++ src/components/Technologies.jsx
@@ -1,95 +1,94 @@
 import { RiReactjsLine, RiJavascriptFill } from 'react-icons/ri';
-import { SiTypescript, SiExpress, SiNestjs, SiSpringboot, SiMysql, SiPrisma, SiMongoose, SiPhp, SiPostgresql, SiGithub, SiDocker, SiKubernetes, SiGit } from 'react-icons/si';
+import {
+  SiTypescript, SiExpress, SiNestjs, SiSpringboot, SiMysql,
+  SiPrisma, SiMongoose, SiPhp, SiPostgresql, SiGithub,
+  SiDocker, SiKubernetes, SiGit
+} from 'react-icons/si';
 import { FaJava } from 'react-icons/fa';
 import { IoLogoAngular } from 'react-icons/io';
 import { BiLogoMongodb } from 'react-icons/bi';
+import { motion } from 'framer-motion';
+
+const iconVariants = duration => ({
+  initial: { y: 0 },
+  animate: {
+    y: [0, -10, 0],
+    transition: {
+      duration,
+      repeat: Infinity,
+      ease: 'easeInOut',
+    }
+  }
+})
+
+
+const icons = {
+  backend: [
+    { Icon: SiExpress, color: 'text-gray-300', duration: 1.5 },
+    { Icon: SiNestjs, color: 'text-pink-600', duration: 2 },
+    { Icon: SiPhp, color: 'text-indigo-600', duration: 2.5 },
+    { Icon: FaJava, color: 'text-orange-600', duration: 3 },
+    { Icon: SiSpringboot, color: 'text-green-500', duration: 3.5 },
+  ],
+  database: [
+    { Icon: BiLogoMongodb, color: 'text-green-600', duration: 1.5 },
+    { Icon: SiPostgresql, color: 'text-blue-500', duration: 2 },
+    { Icon: SiMysql, color: 'text-blue-400', duration: 2.5 },
+    { Icon: SiPrisma, color: 'text-indigo-400', duration: 3 },
+    { Icon: SiMongoose, color: 'text-red-500', duration: 3.5 },
+  ],
+  frontend: [
+    { Icon: RiReactjsLine, color: 'text-cyan-400', duration: 1.5 },
+    { Icon: IoLogoAngular, color: 'text-red-600', duration: 2 },
+    { Icon: RiJavascriptFill, color: 'text-yellow-400', duration: 2.5 },
+    { Icon: SiTypescript, color: 'text-blue-500', duration: 3 },
+  ],
+  tools: [
+    { Icon: SiGit, color: 'text-orange-500', duration: 1.5 },
+    { Icon: SiGithub, color: 'text-white', duration: 2 },
+    { Icon: SiDocker, color: 'text-blue-400', duration: 2.5 },
+    { Icon: SiKubernetes, color: 'text-blue-600', duration: 3 },
+  ]
+};
+
+const Section = ({ title, items }) => (
+  <div>
+    <h3 className="text-2xl text-center my-6">{title}</h3>
+    <motion.div
+      whileInView={{ opacity: 1, x: 0 }}
+      initial={{ opacity: 0, x: -100 }}
+      transition={{ duration: 1.5 }}
+      className="flex flex-wrap items-center justify-center gap-4">
+      {items.map(({ Icon, color, duration }, index) => (
+        <motion.div
+          key={index}
+          variants={iconVariants(duration)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
+          <Icon className={`text-7xl ${color}`} />
+        </motion.div>
+      ))}
+    </motion.div>
+  </div>
+);
 
 export default function Technologies() {
   return (
     <div className="border-b border-neutral-800 pb-24">
-      <h2 className="my-20 text-center text-4xl">Tecnologías</h2>
+      <motion.h2
+        whileInView={{ opacity: 1, x: 0 }}
+        initial={{ opacity: 0, x: -100 }}
+        transition={{ duration: 1.5 }}
+        className="my-20 text-center text-4xl font-bold text-white">
+        Tecnologías y Herramientas
+      </motion.h2>
 
-      {/* Backend */}
-      <div>
-        <h3 className="text-2xl text-center my-6">Backend</h3>
-        <div className="flex flex-wrap items-center justify-center gap-4">
-          <div className="rounded-2xl border-4 border-neutral-800 p-4">
-            <SiExpress className="text-7xl text-gray-300" />
-          </div>
-          <div className="rounded-2xl border-4 border-neutral-800 p-4">
-            <SiNestjs className="text-7xl text-pink-600" />
-          </div>
-          <div className="rounded-2xl border-4 border-neutral-800 p-4">
-            <SiPhp className="text-7xl text-indigo-600" />
-          </div>
-          <div className="rounded-2xl border-4 border-neutral-800 p-4">
-            <FaJava className="text-7xl text-orange-600" />
-          </div>
-          <div className="rounded-2xl border-4 border-neutral-800 p-4">
-            <SiSpringboot className="text-7xl text-green-500" />
-          </div>
-        </div>
-      </div>
-
-      {/* Bases de Datos y ORM */}
-      <div>
-        <h3 className="text-2xl text-center my-6">Base de Datos y ORM</h3>
-        <div className="flex flex-wrap items-center justify-center gap-4">
-          <div className="rounded-2xl border-4 border-neutral-800 p-4">
-            <BiLogoMongodb className="text-7xl text-green-600" />
-          </div>
-          <div className="rounded-2xl border-4 border-neutral-800 p-4">
-            <SiPostgresql className="text-7xl text-blue-500" />
-          </div>
-          <div className="rounded-2xl border-4 border-neutral-800 p-4">
-            <SiMysql className="text-7xl text-blue-400" />
-          </div>
-          <div className="rounded-2xl border-4 border-neutral-800 p-4">
-            <SiPrisma className="text-7xl text-indigo-400" />
-          </div>
-          <div className="rounded-2xl border-4 border-neutral-800 p-4">
-            <SiMongoose className="text-7xl text-red-500" />
-          </div>
-        </div>
-      </div>
-
-      {/* Frontend */}
-      <div>
-        <h3 className="text-2xl text-center my-6">Frontend</h3>
-        <div className="flex flex-wrap items-center justify-center gap-4">
-          <div className="rounded-2xl border-4 border-neutral-800 p-4">
-            <RiReactjsLine className="text-7xl text-cyan-400" />
-          </div>
-          <div className="rounded-2xl border-4 border-neutral-800 p-4">
-            <IoLogoAngular className="text-7xl text-red-600" />
-          </div>
-          <div className="rounded-2xl border-4 border-neutral-800 p-4">
-            <RiJavascriptFill className="text-7xl text-yellow-400" />
-          </div>
-          <div className="rounded-2xl border-4 border-neutral-800 p-4">
-            <SiTypescript className="text-7xl text-blue-500" />
-          </div>
-        </div>
-      </div>
-
-      {/* Herramientas y DevOps */}
-      <div>
-        <h3 className="text-2xl text-center my-6">Herramientas</h3>
-        <div className="flex flex-wrap items-center justify-center gap-4">
-          <div className="rounded-2xl border-4 border-neutral-800 p-4">
-            <SiGit className="text-7xl text-orange-500" />
-          </div>
-          <div className="rounded-2xl border-4 border-neutral-800 p-4">
-            <SiGithub className="text-7xl text-white" />
-          </div>
-          <div className="rounded-2xl border-4 border-neutral-800 p-4">
-            <SiDocker className="text-7xl text-blue-400" />
-          </div>
-          <div className="rounded-2xl border-4 border-neutral-800 p-4">
-            <SiKubernetes className="text-7xl text-blue-600" />
-          </div>
-        </div>
-      </div>
+      <Section title="Backend" items={icons.backend} />
+      <Section title="Base de Datos y ORM" items={icons.database} />
+      <Section title="Frontend" items={icons.frontend} />
+      <Section title="Herramientas" items={icons.tools} />
     </div>
   );
 }
